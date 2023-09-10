2c9b4146971afc8aca5cbd03b55c0786c9f7be5f
seanc0der <sean.noko@gmail.com>
2023-09-10 08:39:16 +0200

    Added a 'handleOpenBookDialog()' function:
    - Amended and updated the previous code relating to the active book preview code logic, and created a function which now includes the rectified code.
    - The function functions perfectly as it opens the book preview dialog after prepending it with the active book's relevant data.
    - Added an click event listener to the books list element.
    - Added commenting on both the function and invocation.

--- scripts.js
+++ scripts.js
@@ -214,7 +214,7 @@ const createPreviewsFragment = (source, indexRange, pageNum) => {
 				src="${image}"
 			/>
 	
-			<div class="preview__info>
+			<div class="preview__info">
 				<h3 class="preview__title">${title}</h3>
 				<div class="preview__author">${authors[authorId]}</div>
 			</div>
@@ -350,83 +350,78 @@ const createAuthorsFragment = (authorsSourceObj) => {
  */
 createAuthorsFragment(authors);
 
-data-list-button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
+// data-search-form.click(filters) {
+//     preventDefault()
+//     const formData = new FormData(event.target)
+//     const filters = Object.fromEntries(formData)
+//     result = []
 
-data-search-form.click(filters) {
-    preventDefault()
-    const formData = new FormData(event.target)
-    const filters = Object.fromEntries(formData)
-    result = []
+//     for (book; booksList; i++) {
+//         titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
+//         authorMatch = filters.author = 'any' || book.author === filters.author
 
-    for (book; booksList; i++) {
-        titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
-        authorMatch = filters.author = 'any' || book.author === filters.author
+//         {
+//             genreMatch = filters.genre = 'any'
+//             for (genre; book.genres; i++) { if singleGenre = filters.genre { genreMatch === true }}}
+//         }
 
-        {
-            genreMatch = filters.genre = 'any'
-            for (genre; book.genres; i++) { if singleGenre = filters.genre { genreMatch === true }}}
-        }
+//         if titleMatch && authorMatch && genreMatch => result.push(book)
+//     }
 
-        if titleMatch && authorMatch && genreMatch => result.push(book)
-    }
+//     if display.length < 1
+//     data-list-message.class.add('list__message_show')
+//     else data-list-message.class.remove('list__message_show')
 
-    if display.length < 1
-    data-list-message.class.add('list__message_show')
-    else data-list-message.class.remove('list__message_show')
+//     data-list-items.appendChild(fragments)
+//     initial === matches.length - [page * BOOKS_PER_PAGE]
+//     remaining === hasRemaining ? initial : 0
+//     data-list-button.disabled = initial > 0
+// 		window.scrollTo({ top: 0, behavior: "smooth" });
+// 		searchDialog.open = false;
 
-    data-list-items.innerHTML = ''
-    const fragment = document.createDocumentFragment()
-    const extracted = source.slice(range[0], range[1])
+// }
 
-    for ({ author, image, title, id }; extracted; i++) {
-        const { author: authorId, id, image, title } = props
+/**
+ * The callback function is triggered by a click event listener when any of the
+ * book element previews from the {@link listItems} is clicked. Before the
+ * {@link listDialog} is displayed using the `showModal()` method, the function
+ * performs some logic to find the `preview id` of the button element.
+ * Afterward, the `books` object is iterated over to find the unique id of the
+ * clicked book. Once found, the unpopulated {@link title}, {@link subtitle},
+ * {@link description}, {@link bookImage}, {@link blur}, and {@link} child
+ * elements of the dialog element are updated with the clicked book's data
+ * before the dialog is shown to the user. If the function doesn't find the
+ * clicked book in the `books` object, it exits via the return keyword.
+ *
+ * @param {Event} event
+ */
+const handleOpenBookDialog = (event) => {
+	const pathArray = event.composedPath();
+	let activeBook = undefined;
 
-        element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
+	for (const node of pathArray) {
+		if (activeBook) break;
 
-        element.innerHTML = /* html */ `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
+		const dataKeyExists = "preview" in node.dataset;
+		const previewId = dataKeyExists && node.dataset.preview;
 
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[authorId]}</div>
-            </div>
-        `
+		for (const book of books) {
+			if (book.id === previewId) activeBook = book;
+		}
+	}
 
-        fragment.appendChild(element)
-    }
+	if (!activeBook) return;
 
-    data-list-items.appendChild(fragments)
-    initial === matches.length - [page * BOOKS_PER_PAGE]
-    remaining === hasRemaining ? initial : 0
-    data-list-button.disabled = initial > 0
+	const publishYear = new Date(activeBook.published).getFullYear();
+	const author = authors[activeBook.author];
 
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    data-search-overlay.open = false
-}
+	title.textContent = activeBook.title;
+	subtitle.textContent = `${author} ${publishYear}`;
+	description.textContent = activeBook.description;
+	bookImage.setAttribute("src", activeBook.image);
+	blur.setAttribute("src", activeBook.image);
 
-data-list-items.click() {
-    pathArray = Array.from(event.path || event.composedPath())
-    active;
+	listDialog.showModal();
+};
 
-    for (node; pathArray; i++) {
-        if active break;
-        const previewId = node?.dataset?.preview
-
-        for (const singleBook of books) {
-            if (singleBook.id === id) active = singleBook
-        }
-    }
-
-    if !active return
-    data-list-active.open === true
-    data-list-blur + data-list-image === active.image
-    data-list-title === active.title
-
-    data-list-subtitle === '${authors[active.author]} (${Date(active.published).year})'
-    data-list-description === active.description
-}
+listItems.addEventListener("click", handleOpenBookDialog);
